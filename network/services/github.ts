9f4fdb0466792f751c0d1ec9cc5192133a8a3b89
dawid-bytys <dawid.bytys@proton.me>
2024-08-08 00:06:06 +0200

    refactor: service

--- network/services/github.ts
+++ network/services/github.ts
@@ -2,6 +2,8 @@ import { githubClient } from '../interceptors';
 import type { RepositoriesResponse } from '../responses/repos';
 import type { UsersResponse } from '../responses/users';
 
+const BASE_URL = 'https://api.github.com';
+
 interface RepositoriesParams {
   username: string;
   pageParam: number;
@@ -10,14 +12,14 @@ interface RepositoriesParams {
 export const GitHubService = {
   fetchUsers: async (query: string) => {
     const response = await githubClient.get<UsersResponse>(
-      `https://api.github.com/search/users?q=${query}&per_page=5`,
+      `${BASE_URL}/search/users?q=${query}&per_page=5`,
     );
     return response.data;
   },
 
   fetchRepositories: async ({ username, pageParam }: RepositoriesParams) => {
     const response = await githubClient.get<RepositoriesResponse[]>(
-      `https://api.github.com/users/${username}/repos?per_page=5&page=${pageParam}`,
+      `${BASE_URL}/users/${username}/repos?per_page=5&page=${pageParam}`,
     );
     return response.data;
   },
